585381ae48736dfacd75fece80eb3aae0e6c5154
valnoxy <jonas@exploitox.de>
2023-03-23 23:31:42 +0100

    🧹 Cleanup code

--- deploya/deployaUI/Common/DebugConsole.cs
+++ deploya/deployaUI/Common/DebugConsole.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
-using System.Threading;
 using deployaCore.Common;
 
 namespace deployaUI.Common
@@ -24,7 +23,20 @@ namespace deployaUI.Common
 
         public static void WriteLine(string message, ConsoleColor color = ConsoleColor.White)
         {
-            if (_logConsole == null) return;
+            if (_logConsole == null)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("[");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("*");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("] ");
+
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                return;
+            }
+
             _logConsole.ForegroundColor = ConsoleColor.DarkGray;
             _logConsole.Write("[");
             _logConsole.ForegroundColor = ConsoleColor.White;
@@ -38,7 +50,22 @@ namespace deployaUI.Common
 
         public static void Write(string message, bool continueLine = false, ConsoleColor color = ConsoleColor.White)
         {
-            if (_logConsole == null) return;
+            if (_logConsole == null)
+            {
+                if (!continueLine)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.Write("[");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("*");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.Write("] ");
+                }
+
+                Console.ForegroundColor = color;
+                Console.Write(message);
+                return;
+            }
 
             if (!continueLine)
             {
@@ -54,39 +81,9 @@ namespace deployaUI.Common
             _logConsole.Write(message);
         }
 
-        public static void TestWriteAction()
-        {
-            if (_actionConsole == null) return;
-
-            var pb = new ProgressBar(_actionConsole, PbStyle.SingleLine, 50);
-            pb.Refresh(0, "connecting to server to download 5 files asychronously.");
-            Thread.Sleep(1000);
-            pb.Refresh(25, "downloading file number 25");
-            Thread.Sleep(1000);
-            pb.Refresh(50, "finished.");
-            var pb1 = new ProgressBar(_actionConsole, PbStyle.SingleLine, 50);
-            pb1.Refresh(0, "connecting to server to download 5 files asychronously.");
-            Thread.Sleep(1000);
-            pb1.Refresh(25, "downloading file number 25");
-            Thread.Sleep(1000);
-            pb1.Refresh(50, "finished.");
-            var pb2 = new ProgressBar(_actionConsole, PbStyle.SingleLine, 50);
-            pb2.Refresh(0, "connecting to server to download 5 files asychronously.");
-            Thread.Sleep(1000);
-            pb2.Refresh(25, "downloading file number 25");
-            Thread.Sleep(1000);
-            pb2.Refresh(50, "finished.");
-            var pb3 = new ProgressBar(_actionConsole, PbStyle.SingleLine, 50);
-            pb3.Refresh(0, "connecting to server to download 5 files asychronously.");
-            Thread.Sleep(1000);
-            pb3.Refresh(25, "downloading file number 25");
-            Thread.Sleep(1000);
-            pb3.Refresh(50, "finished.");
-        }
-
         public static void RefreshProgressBar(Progress progress, int value, string action)
         {
-            ProgressBar progressBar = null!;
+            ProgressBar progressBar;
             switch (progress)
             {
                 case Progress.PrepareDisk:
@@ -132,15 +129,34 @@ namespace deployaUI.Common
         {
             Console.Title = $@"{_versionInfo.ProductName} - Debug Console";
 
-            // create an 80 by 20 inline window
-            var window = new Window(Console.WindowWidth, Console.WindowHeight -1);
-            var consoles = window.SplitRows(
-                new Split(0, "Log", LineThickNess.Single),
-                new Split(8, "Action", LineThickNess.Single)
-            );
+            try
+            {
+                var window = new Window(Console.WindowWidth, Console.WindowHeight - 1);
+                var consoles = window.SplitRows(
+                    new Split(0, "Log", LineThickNess.Single),
+                    new Split(8, "Action", LineThickNess.Single)
+                );
 
-            _logConsole = consoles[0];
-            _actionConsole = consoles[1];
+                _logConsole = consoles[0];
+                _actionConsole = consoles[1];
+            }
+            catch (System.ArgumentOutOfRangeException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("[");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("*");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("] ");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Error: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("The console window is too small. Please resize the window and try again.");
+                Environment.Exit(1);
+            }
+            
+
 
             _logConsole.WriteLine($"{_versionInfo.ProductName} [Version: {_versionInfo.ProductVersion}]"); // Header
             _logConsole.WriteLine(_versionInfo.LegalCopyright + "\n"); // Copyright text
