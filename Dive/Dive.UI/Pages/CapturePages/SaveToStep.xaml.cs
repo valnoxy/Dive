1a6bb02bd16f0452cf05a0e6d0ea4aaf01ec7c65
valnoxy <jonas@exploitox.de>
2023-12-11 23:34:03 +0100

    Update Translation

--- Dive/Dive.UI/Pages/CapturePages/SaveToStep.xaml.cs
+++ Dive/Dive.UI/Pages/CapturePages/SaveToStep.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using Dive.UI.Common;
 
 namespace Dive.UI.Pages.CapturePages
 {
@@ -37,21 +38,25 @@ namespace Dive.UI.Pages.CapturePages
         private void LoadDisks()
         {
             disks = new List<Disk>();
-            int counter = 0;
+            var counter = 0;
 
             // Find WIM USB device 
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
-            foreach (DriveInfo d in allDrives)
+            var allDrives = DriveInfo.GetDrives();
+            foreach (var d in allDrives)
             {
                 if (File.Exists(Path.Combine(d.Name, ".diveusb")) && Directory.Exists(Path.Combine(d.Name, "WIMs")))
                 {
-                    Output.WriteLine($"Dive Disk detected: " + d.Name);
+                    Debug.WriteLine("Dive Disk detected: " + d.Name);
                     disks.Add(new Disk { Picture = "pack://application:,,,/assets/icon-hdd-40.png", Path = $"{d.Name}WIMs" });
                     counter++;
                 }
             }
 
-            ImageCounter.Text = $"Dive Disks loaded: {counter}";
+            var localizedImageCounter = (string)Application.Current.MainWindow!.FindResource("CaptureSaveToDisksCounter");
+            if (string.IsNullOrEmpty(localizedImageCounter))
+                localizedImageCounter = "Dive Disks loaded: {0}";
+
+            ImageCounter.Text = string.Format(localizedImageCounter, counter);
             this.DataContext = this;
         }
 
